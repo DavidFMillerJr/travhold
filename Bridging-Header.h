ccab23d1f548e4fbf4df5d6f16c8a6798785858f
David F. Miller Jr <41369663+DavidFMillerJr@users.noreply.github.com>
2018-07-18 11:37:22 -0400

    Add files via upload

---
+++ Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  Chat
+//
+//  Created by Cindy Lin on 7/18/18.
+//  Copyright © 2018 Cindy Lin. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#endif /* Bridging_Header_h */
+
+
